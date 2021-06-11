3c32b90ce9d62ab7216f5dd4baf287c4393041ac
Osama-Rais <77352260+Osama-Rais@users.noreply.github.com>
2021-06-11 02:23:31 -0700

    uiicons

--- api/auth/main.py
+++ api/auth/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:3001",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@material-ui/icons"
+// import "@material-ui/core"
+// import "@fortawesome/free-solid-svg-icons"
+// import "@fortawesome/free-solid-svg-icons"
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "App.js"
 
 
